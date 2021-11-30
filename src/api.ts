6e67c98e8ffabe5b646febc7d10530954dec30e8
Samara Hurtado Ramos <66747991+Miss-Dev@users.noreply.github.com>
2021-11-30 00:20:05 -0400

    Delete api.ts

--- src/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-/**
- * Configurar o axios para a api: https://jsonplaceholder.typicode.com/
- */
-export default axios.create({
-    baseURL: `http://jsonplaceholder.typicode.com/`
-});
