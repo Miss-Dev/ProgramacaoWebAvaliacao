769e92fcf288a2221dfa44f290f1c2f066da9151
Miss Dev <samaraprogramadoralegal@outlook.com>
2021-11-30 00:34:10 -0400

    Etapa 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import API from './api'
 
 function App() { 
    
-    
+    //imprimindo response(inteiro) no console    
+    API.get('/posts')
+    .then(resposta => console.log(resposta))  
+ 
+
 
     return (
     
