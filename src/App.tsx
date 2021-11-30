7f438e73bde1bb91d1a33486131ff2398909dda4
Miss Dev <samaraprogramadoralegal@outlook.com>
2021-11-30 02:48:12 -0400

    Etapa 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import { AxiosResponse } from 'axios';
+import React, { useState } from 'react';
 import API from './api'
 
 
+async function getRep(){
+    const resp = await API.get('/posts');
+    return resp.data
+}
 
 function App() { 
-   
+    const [posts, setPosts] = useState({});
+    
     //imprimindo response(inteiro) no console    
-    API.get('/posts')
-    .then(resposta => console.log(resposta))  
- 
-
-
+    // API.get('/posts')
+    // .then(resposta => console.log(resposta.data));
+    
+    console.log('*****************posts**********************')
+    
+    console.log(posts)
     return (
+        <>
     
         <div>Comece aqui!</div>
-        
+        <button onClick={() => setPosts({posts: getRep()})}>
+            Pegar postagens
+        </button>
+    
+        </>
        
     );
+    
    
 }
 
 export default App;
+
