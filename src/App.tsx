1d12e547b3c8ee40376adfc68df9d306ab74b4f9
Miss Dev <samaraprogramadoralegal@outlook.com>
2021-11-30 23:50:07 -0400

    Etapa 7

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,33 @@ function App() {
         API.get('/posts')
         .then(resposta => setPosts(resposta.data));
     }
-
-    console.log('*****************posts**********************')
     
-    console.log(posts)
+    console.log(imprimir())
+    console.log('*****************posts**********************');
+    let userId = [];
+    //user ids
+    userId = posts.map((obje: { [s: string]: unknown; } | ArrayLike<unknown>) => {
+        return Object.values(obje)[0];
+    })
     
+//    posts.filter(p => Object.values(p)[0]);
+    console.log(userId);
+   
 
     return (
+        
         <>
     
         <div>Comece aqui!</div>
         <button onClick={adiciona}>
             Pegar postagens
         </button>
+
+        <select>
+        {
+           <option value={userId.values}></option> 
+        }
+        </select>     
 
         
     
