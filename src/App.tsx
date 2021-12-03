6495b2b739bc27fe25cd4dda0a292a4fad0ae057
Miss Dev <samaraprogramadoralegal@outlook.com>
2021-12-03 00:36:27 -0400

    Etapa 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import API from './api'
+import React, { Component, useState } from 'react';
+import { isTemplateExpression } from 'typescript';
+import API from './api';
+import './index.css'
 
 
+function App(){ 
 
-
-function App() { 
     const [posts, setPosts] = useState([]);
-
+   
     
     //imprimindo response(inteiro) no console 
     function imprimir(){
@@ -14,42 +15,85 @@ function App() {
             .then(resposta => console.log(resposta.data));
     }
     
-    //funcao para operacao com a variavel de estado
+    //funcao para operacao com a variavel de estado e imprime os posts
     function adiciona(){  
         API.get('/posts')
         .then(resposta => setPosts(resposta.data));
+        console.log(imprimir())
     }
     
-    console.log(imprimir())
-    console.log('*****************posts**********************');
-    let userId = [];
-    //user ids
-    userId = posts.map((obje: { [s: string]: unknown; } | ArrayLike<unknown>) => {
-        return Object.values(obje)[0];
-    })
     
-//    posts.filter(p => Object.values(p)[0]);
-    console.log(userId);
    
+    const arr = posts.map((e) => {
+        return String(Object.values(e)[0]);
+    })
+    var userId = arr.filter((este, i) => arr.indexOf(este) === i);
+    //user ids
+    function pegaUserId(){
+      
+        let userIds: { label: string; value: string; }[]= []
+          //transforma em value e labels
+        userId.forEach(function(element) {
+            userIds.push({ label:element, value: element })
+        });
+        return userIds;
+    }
 
+  
+    
+    const [selectedOption, setSelectedOption] = useState(userId[0]);
+    let a = 0;
+   
+//    posts.filter(p => Object.values(p)[0]);
+ 
+    
     return (
-        
-        <>
-    
+    <>
         <div>Comece aqui!</div>
-        <button onClick={adiciona}>
-            Pegar postagens
-        </button>
-
-        <select>
-        {
-           <option value={userId.values}></option> 
-        }
-        </select>     
-
+        <div>
+            Clique aqui para pegar as postagens: 
+            <button onClick={adiciona}>
+                Pegar postagens
+            </button>
+            
+        </div>
+        <br/>
+        {/* mostrando userIds */}
+        <label>Selecione um userId para ver a postagem: </label>
+        <select onChange={e => setSelectedOption(e.target.value)}>
+            <option value="0">Selecione</option>
+        {pegaUserId().map(item => {
+                a = 0
+                return (                      
+                                   
+                    <option key={item.label || "Aqui"} value={item.value}>{item.value}</option>                          
+                    
+                )
+                
+                
+            
+        })      
+              
+        }        
+      
+        </select>
+          {/* mostrando de acordo com userId selecionado*/}
+        <p></p>
+        {       
+        posts.map((e) => {
+            if (String(Object.values(e)[0]) === selectedOption && a === 0){
+                //condição de parada para não pegar mais de uma postagem por userId
+                a = 1
+                return(
+                    <><h2>Título: {String(Object.values(e)[2])}<br/></h2><p> Texto: {String(Object.values(e)[3])}</p></>
+                );
+                
+            }
+        })}
+        
         
     
-        </>
+    </>
        
     );
     
@@ -57,4 +101,6 @@ function App() {
 }
 
 export default App;
+
+
 
